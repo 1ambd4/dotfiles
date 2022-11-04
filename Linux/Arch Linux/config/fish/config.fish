79571ad3594f81a44fd82c724733dbee2ed5bb15
lttzz <luckdog.cdr@gmail.com>
2022-11-04 18:17:52 +0800

    backup ~/.config/fish/config.fish

---
+++ Linux/Arch Linux/config/fish/config.fish
@@ -0,0 +1,16 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias clash "~/.config/clash/clash"
+
+alias pbcopy 'xclip -selection clipboard'
+alias pbpaste 'xclip -selection clipboard -o'
+
+set -x PATH /usr/local/go/bin $PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/miniconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
